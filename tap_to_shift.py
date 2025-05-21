e3d63e208dadc25f9c6ae10d91dbd03306dd5988
sheilamaebalaga <smtamondong@gmail.com>
2025-05-21 08:59:28 -0400

    Update tap_to_shift.py

--- tap_to_shift.py
+++ tap_to_shift.py
@@ -21,7 +21,7 @@ if "step" not in st.session_state:
 
 # Welcome screen
 if st.session_state.step == 0:
-    st.markdown("<h1 style='text-align: center;'>Tap to Shift</h1>", unsafe_allow_html=True)
+   st.markdown("<h1 style='text-align: center;'>Tap to Shift</h1>", unsafe_allow_html=True)
 
    st.write("""
 The 8A Method is a moment-to-moment energetic shift.  
