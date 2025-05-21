dad113ae5146be8db581c7578d05c378bed173db
sheilamaebalaga <smtamondong@gmail.com>
2025-05-21 09:02:21 -0400

    Update tap_to_shift.py

--- tap_to_shift.py
+++ tap_to_shift.py
@@ -23,13 +23,15 @@ if "step" not in st.session_state:
 if st.session_state.step == 0:
    st.markdown("<h1 style='text-align: center;'>Tap to Shift</h1>", unsafe_allow_html=True)
 
-   st.write("""
+  st.write("""
 The 8A Method is a moment-to-moment energetic shift.  
 Whether you're navigating something heavy, uncertain, joyful, or overwhelming, this method helps you slow down, tune in, discern, and realign.  
 It’s not about fixing how you feel. It’s about being fully present with it, so something deeper can emerge—tuning into better outcomes that support your well-being.
 """)
-    st.markdown("---")
-    st.subheader("The 8 Steps")
+
+st.markdown("---")
+st.subheader("The 8 Steps")
+
 
     st.markdown("""
 - **Awareness** – Recognize what situation or moment is stirring something inside you.  
